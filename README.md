a33ed6caa4c329d85381395c01d47c093e49a647
unknown <miguel.motta-u@labinfo.is.escuelaing.edu.co>
2025-10-22 10:29:22 -0500

    feat: update readme and fix api response

--- README.md
+++ README.md
@@ -9,4 +9,22 @@ AREP T2 Exam
 ### Testing Cient
 ![](assets/img_1.png)
 
-### Deploying in AWS
+## Deploying in AWS
+### Configuration
+
+![](assets/img_2.png)
+![](assets/img_5.png)
+### Set up EC2
+````shell
+sudo yum install java -y
+sudo yum install maven -y
+yum install git -y
+git clone https://github.com/MIGUEL-MOTTA-U/AREP-Exam-T2
+cd ./AREP-Exam-T2
+mvn clean package
+mvn spring-boot:run
+````
+![](assets/img_3.png)
+
+![](assets/img_4.png)
+
--- src/main/java/com/arep/test/miguel/models/ApiResponse.java
+++ src/main/java/com/arep/test/miguel/models/ApiResponse.java
@@ -15,34 +15,4 @@ public class ApiResponse {
     private String operation;
     private int input;
     private List<Integer> output;
-
-    public ApiResponse(String op, int value, List<Integer> result) {
-        this.input = value;
-        this.operation = op;
-        this.output = result;
-    }
-
-    public String getOperation() {
-        return operation;
-    }
-
-    public void setOperation(String operation) {
-        this.operation = operation;
-    }
-
-    public int getInput() {
-        return input;
-    }
-
-    public void setInput(int input) {
-        this.input = input;
-    }
-
-    public List<Integer> getOutput() {
-        return output;
-    }
-
-    public void setOutput(List<Integer> output) {
-        this.output = output;
-    }
 }
