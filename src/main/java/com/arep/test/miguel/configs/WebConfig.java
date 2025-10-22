efbebe4e8c6cb8577276c68ceeb4e9872225a666
unknown <miguel.motta-u@labinfo.is.escuelaing.edu.co>
2025-10-22 11:14:43 -0500

    feat: update configuration to handle proxy server (with active and passive)

--- src/main/java/com/arep/test/miguel/configs/WebConfig.java
+++ src/main/java/com/arep/test/miguel/configs/WebConfig.java
@@ -2,17 +2,31 @@ package com.arep.test.miguel.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.Duration;
 
 @Configuration
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*")
+                .maxAge(3600);
+    }
+
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**")
                 .addResourceLocations("/public", "classpath:/static/")
                 .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
     }
+
+
 }
--- src/main/java/com/arep/test/miguel/controllers/CalculatorController.java
+++ src/main/java/com/arep/test/miguel/controllers/CalculatorController.java
@@ -2,11 +2,10 @@ package com.arep.test.miguel.controllers;
 
 import com.arep.test.miguel.models.ApiResponse;
 import com.arep.test.miguel.services.interfaces.CalculatorService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
 
@@ -15,9 +14,15 @@ import java.util.List;
 @RequestMapping("/api/calculator")
 public class CalculatorController {
     private final CalculatorService calculatorService;
+    private final boolean isProxy = false;
 
-    public CalculatorController(CalculatorService calculatorService) {
+    private final RestClient restClient;
+    private final RestClient pasiveClient;
+
+    public CalculatorController(CalculatorService calculatorService, RestClient.Builder restClientBuilder) {
         this.calculatorService = calculatorService;
+        this.restClient = restClientBuilder.baseUrl("").build();
+        this.pasiveClient = restClientBuilder.baseUrl("").build();
     }
 
     @GetMapping("/health")
@@ -27,6 +32,14 @@ public class CalculatorController {
 
     @GetMapping("/tribseq")
     public ResponseEntity<ApiResponse> tribonacciSeq(@RequestParam int value) {
+        if (isProxy) {
+            try {
+                return ResponseEntity.status(200).body(this.restClient.get().uri("/api/calculator/tribseq?value="+value).retrieve().body(ApiResponse.class));
+            } catch (Exception e) {
+                return ResponseEntity.status(200).body(this.pasiveClient.get().uri("/api/calculator/tribseq?value="+value).retrieve().body(ApiResponse.class));
+            }
+
+        }
         List<Integer> result = calculatorService.calculateTribonacci(value);
         return ResponseEntity.status(200).body(new ApiResponse("Secuencia de Tribonacci", value, result));
     }
